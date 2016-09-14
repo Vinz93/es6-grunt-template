4996525e00f69d885e6a754dc18195705127229f
vincenzo <vincenzobianco1993@gmail.com>
2016-09-14 14:46:31 -0400

    validations for facebookV2 route

--- config/param_validations/player.js
+++ config/param_validations/player.js
@@ -1,35 +1,45 @@
 import Joi from 'joi';
 
 export default {
-  create: {
-    body: {
-      email: Joi.string().email().required(),
-      password: Joi.string().required(),
-      firstName: Joi.string().required(),
-      lastName: Joi.string().required(),
-      gender: Joi.string().required(),
-      bornAt: Joi.string().isoDate().required(),
+    create: {
+        body: {
+            email: Joi.string().email().required(),
+            password: Joi.string().required(),
+            firstName: Joi.string().required(),
+            lastName: Joi.string().required(),
+            gender: Joi.string().required(),
+            bornAt: Joi.string().isoDate().required(),
+        },
     },
-  },
-  facebook: {
-    body: {
-      email: Joi.string().email().required(),
-      firstName: Joi.string().required(),
-      lastName: Joi.string().required(),
-      gender: Joi.string().required(),
-      bornAt: Joi.string().isoDate().required(),
-      facebookId: Joi.string().required(),
-    }
-  },
-  twitter: {
-    body: {
-      email: Joi.string().email().required(),
-      firstName: Joi.string().required(),
-      lastName: Joi.string().required(),
-      gender: Joi.string().required(),
-      bornAt: Joi.string().isoDate().required(),
-      twitterId: Joi.string().required(),
-    }
-  }
+    facebook: {
+        body: {
+            email: Joi.string().email().required(),
+            firstName: Joi.string().required(),
+            lastName: Joi.string().required(),
+            gender: Joi.string().required(),
+            bornAt: Joi.string().isoDate().required(),
+            facebookId: Joi.string().required(),
+        }
+    },
+    twitter: {
+        body: {
+            email: Joi.string().email().required(),
+            firstName: Joi.string().required(),
+            lastName: Joi.string().required(),
+            gender: Joi.string().required(),
+            bornAt: Joi.string().isoDate().required(),
+            twitterId: Joi.string().required(),
+        }
+    },
+    facebookV2: {
+        body: {
+            email: Joi.string().email(),
+            firstName: Joi.string().required(),
+            lastName: Joi.string(),
+            gender: Joi.string(),
+            bornAt: Joi.string().isoDate(),
+            facebookId: Joi.string().required(),
+        }
+    },
 
 };
